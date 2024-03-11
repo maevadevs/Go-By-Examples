f7c2b9abf99ab649ca5afede5cd58753300729b8
maevadevs <maevadevs@users.noreply.github.com>
2024-03-11 17:48:10 -0500

    Add 27.channel-buffering

---
+++ 27.channel-buffering/go.mod
@@ -0,0 +1,3 @@
+module ralafi.com/go/go-by-examples/27.channel-buffering
+
+go 1.22.1
---
+++ 27.channel-buffering/src/main.go
@@ -0,0 +1,38 @@
+// Package
+// *******
+package main
+
+// Imports
+// *******
+import "fmt"
+
+// main
+// ****
+// Default channels are unbuffered
+// THey only accept sends (chan <-) if there is a corresponding receive (<- chan) ready to receive the sent value
+// Buffered channels accept a limited number of values without a corresponding receiver for those values
+
+func main() {
+    // A channel of strings buffering up to 2 values
+    messages := make(chan string, 2)
+
+    // We can send values into the channel without a corresponding concurrent receive
+    messages <- "buffered"
+    messages <- "channel"
+
+    // We can receive those values later as usual
+    fmt.Println(<-messages)
+    fmt.Println(<-messages)
+}
+
+// FOR WINDOWS:
+//  To run:                 go run 27.channel-buffering\src\main.go
+//  To compile:             go build -o 27.channel-buffering\bin\channel-buffering.exe 27.channel-buffering\src\main.go
+//  To run after compile:   .\27.channel-buffering\bin\channel-buffering.exe
+//  Compile + Run:          go build -o 27.channel-buffering\bin\channel-buffering.exe 27.channel-buffering\src\main.go && .\27.channel-buffering\bin\channel-buffering.exe
+
+// FOR LINUX:
+//  To run:                 go run 27.channel-buffering/src/main.go
+//  To compile:             go build -o 27.channel-buffering/bin/channel-buffering 27.channel-buffering/src/main.go
+//  To run after compile:   ./27.channel-buffering/bin/channel-buffering
+//  Compile + Run:          go build -o 27.channel-buffering/bin/channel-buffering 27.channel-buffering/src/main.go && ./27.channel-buffering/bin/channel-buffering
