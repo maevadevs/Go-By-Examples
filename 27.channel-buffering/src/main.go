3fba5ec3886c6daf15b212fc26c6a336f7065d28
maevadevs <maevadevs@users.noreply.github.com>
2024-03-14 23:13:11 -0500

    Fix typo

--- 27.channel-buffering/src/main.go
+++ 27.channel-buffering/src/main.go
@@ -9,7 +9,7 @@ import "fmt"
 // main
 // ****
 // Default channels are unbuffered
-// THey only accept sends (chan <-) if there is a corresponding receive (<- chan) ready to receive the sent value
+// They only accept sends (chan <-) if there is a corresponding receive (<- chan) ready to receive the sent value
 // Buffered channels accept a limited number of values without a corresponding receiver for those values
 
 func main() {
